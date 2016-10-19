29311959b7cfbad03f2cf526e8d662556741235e
wecohr <weco8.ri@gmail.com>
2016-10-20 01:47:22 +0200

    Concat JavaScript

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,28 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
 
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
 
-gulp.task('coffee', function () {
+var jsSources = [
+  'componets/scripts/rclick.js',
+  'componets/scripts/pixgrid.js',
+  'componets/scripts/tagline.js',
+  'componets/scripts/template.js'
+];
+
+gulp.task('coffee', function() {
     gulp.src(coffeeSources)
       .pipe(coffee({bare: true})
         .on('error', gutil.log))
         .pipe(gulp.dest('components/scripts'))
+});
+
+//Concat - spanjanje gore navedenih js fileova u jedan script.js
+gulp.task('js', function() {
+  gulp.src(jsSources)
+  .pipe(concat('script.js'))
+  .pipe(gulp.dest('builds/development/js'))
 });
