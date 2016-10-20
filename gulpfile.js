f785dc817954764841d393f747ac0c67011911ed
wecohr <weco8.ri@gmail.com>
2016-10-20 02:15:38 +0200

    Dodan COMPASS proces (SASS)

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
-    browserify = require('gulp-browserify');
+    browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
     concat = require('gulp-concat');
 
 
@@ -14,6 +15,9 @@ var jsSources = [
   'componets/scripts/template.js'
 ];
 
+var sassSources = ['components/sass/style.scss'];
+
+
 gulp.task('coffee', function() {
     gulp.src(coffeeSources)
       .pipe(coffee({bare: true})
@@ -21,10 +25,22 @@ gulp.task('coffee', function() {
         .pipe(gulp.dest('components/scripts'))
 });
 
+
 //Concat - spanjanje gore navedenih js fileova u jedan script.js
 gulp.task('js', function() {
   gulp.src(jsSources)
   .pipe(concat('script.js'))
   .pipe(browserify())
   .pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('compass', function() {
+  gulp.src(sassSources)
+  .pipe(compass({
+    sass: 'components/sass',
+    image: 'builds/development/images',
+    style: 'expanded'
+  }))
+  .on('error', gutil.log)
+  .pipe(gulp.dest('builds/development/css'))
 });
